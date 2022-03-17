d6f325ec7f161ef8181f09caf1207d1fd1e9d793
palrohitg <rohitprotobit@gmail.com>
2022-03-17 23:25:08 +0530

    hot fix

--- CRUD.sql
+++ CRUD.sql
@@ -19,6 +19,18 @@ ALTER TABLE users MODIFY id INTEGER NOT NULL AUTO_INCREMENT;
 Alter table POST modify Id integer NOT NULL AUTO_INCREMENT;
 
 
+-- Create commands with foreign keys 
+create table student (
+    id int not null auto_increment, 
+    name varchar(255) not null,
+    age int varchar(255) not null, 
+    city varchar(255) not null,
+    primary key (id), 
+    foreign key(city) references City(cid)
+); 
+
+-- Homepage Issue fixed 
+SELECT post_title,count(posts.post_id) as Total FROM posts JOIN comments WHERE posts.post_id = comments.post_id GROUP BY posts.post_id
 
 
 
@@ -28,7 +40,40 @@ Primary Keys :
     1. always have the unique data 
     2. Primary keys can't store the NULL values. 
     3. But the unique can store the NULL values.         
+    4. One table can have only one primary keys and one table can have multiple unique keys 
+
+Foreign keys : 
+   1. Those values if the becomes the primary keys in some other tables.
+
+-- Group by always used with function 
 
 
 */
 
+-- Group by function always used with funct ions 
+-- Basically we are getting muliple records then we are creating new group. 
+SELECT count(p.Id) from POST p 
+inner join Comment c 
+on p.Id = c.post_id GROUP by p.Id;
+
+
+-- Remember groupby always used with having here we can't use having persons here. 
+SELECT countryName, SUM(population) as population
+from table 
+group by countryName
+having sum(population) > 1000; 
+
+
+-- Subquery and Nested Query 
+-- Many time there is nested chain when output of one query is send to other query. 
+select * from emptable where id in 
+(select id from empsal where salary > 150);
+
+-- 2nd Highest Salary by using the co-related queries. 
+
+
+-- What are indexes in 
+select emp_name from emp_sal e1
+where 2 =
+(select count(e2.salary)
+from empsal e2 where e2.salary >= e1.salary);
